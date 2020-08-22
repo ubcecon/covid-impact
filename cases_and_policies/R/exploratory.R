c59856b3f7a4702fd03fdab460d5608526453570
Hiro Kasahara <hkasahar@gmail.com>
2020-08-21 17:48:03 -0700

    update

--- cases_and_policies/R/exploratory.R
+++ cases_and_policies/R/exploratory.R
@@ -17,7 +17,33 @@ colors <-  scale_color_solarized
 
 cs <- covidstates
 cs <- cs[cs$fips<60, ] # drop territories (Puerto Rico, Guam, etc)
+# cs <- cs[cs$date<=as.Date("2020-06-03"), ] # drop territories (Puerto Rico, Guam, etc)
 cs <- cs[order(cs$state, cs$date),]
+ 
+quantileplot <- function(v, p=c(0.05, 0.25, 0.5, 0.75, 0.95), data=cs) {
+  data$y <- data[,v]
+  adf <- aggregate(y ~ date, data=data,
+                   FUN=function(x) quantile(x, probs=p, na.rm=TRUE))
+  p <- as.factor(p)
+  s <- 1.2
+  txt <- data.frame(date=max(adf$date), y=adf$y[nrow(adf$y),], p=p)
+  names(adf)[1] <- "date"
+  figm <- ggplot(adf) +
+    geom_line(aes(x=date, y=adf$y[,1], color=p[1]), size=s) +
+    geom_line(aes(x=date, y=adf$y[,2], color=p[2]), size=s) +
+    geom_line(aes(x=date, y=adf$y[,3], color=p[3]), size=s) +
+    geom_line(aes(x=date, y=adf$y[,4], color=p[4]), size=s) +
+    geom_line(aes(x=date, y=adf$y[,5], color=p[5]), size=s) +
+    geom_text(data=txt,aes(x=date,y=y,color=p, label=p,
+                           hjust="left",vjust="center", size=2)) +
+    plottheme +
+    colors() + 
+    #ggtitle(TeX(paste("Evolution of", varlabels[varlabels$Variable==v,"Label"]))) +
+    geom_line(data=data, aes(x=date, y=y, group=state), alpha=0.2) +
+    theme(legend.position="none") +
+     labs(color='Quantile')  #+
+    # xlim(startday, max(data$date))
+}
 
 ################################################################################
 ## Case Measures
@@ -58,6 +84,16 @@ fjhu <- ggplot(cs, aes(x=date, y=newcases.jhu, colour=state)) + geom_line(alpha=
 pdf(paste(outdir,"newcases.pdf", sep="/"))
 grid.arrange(fnyt,fjhu,fctp)
 dev.off()
+
+fnyt_daily <- quantileplot("newcases.nyt", data=cs) + ylab("Daily new cases") + ggtitle("NYT Daily New Cases") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+ 
+fctp <- quantileplot("newcases.ctp", data=cs) + ylab("New cases") + ggtitle("CTP") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03")) # +  xlim(as.Date("2020-03-10"),Sys.Date())
+fnyt <- quantileplot("newcases.nyt", data=cs) + ylab("New cases") + ggtitle("NYT") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+fjhu <- quantileplot("newcases.jhu", data=cs) + ylab("New cases") + ggtitle("JHU") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+pdf(paste(outdir,"newcases_q.pdf", sep="/")) 
+grid.arrange(fnyt,fjhu,fctp)
+dev.off()
+
 
 X  <- cs[,c("newcases.nyt","newcases.jhu","newcases.ctp")]
 X[X<0] <- NA
@@ -103,6 +139,18 @@ fjhu <- ggplot(cs, aes(x=date, y=newcases.jhu, colour=state)) + geom_line(alpha=
 pdf(paste(outdir,"weekcases.pdf", sep="/"))
 grid.arrange(fnyt,fjhu,fctp)
 dev.off()
+ 
+
+fnyt_weekly <- quantileplot("newcases.nyt", data=cs) + ylab("Weekly new cases") + ggtitle("NYT Weekly Cases") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+
+
+fctp <- quantileplot("newcases.ctp", data=cs) + ylab("New cases") + ggtitle("CTP") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03")) # +  xlim(as.Date("2020-03-10"),Sys.Date())
+fnyt <- quantileplot("newcases.nyt", data=cs) + ylab("New cases") + ggtitle("NYT") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+fjhu <- quantileplot("newcases.jhu", data=cs) + ylab("New cases") + ggtitle("JHU") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+pdf(paste(outdir,"weekcases_q.pdf", sep="/")) 
+grid.arrange(fnyt,fjhu,fctp)
+dev.off()
+
 
 ################################################################################
 ## Deaths
@@ -136,6 +184,17 @@ pdf(paste(outdir,"weekdeaths.pdf", sep="/"))
 grid.arrange(fnyt,fjhu,fctp)
 dev.off()
 
+fnyt_death_weekly <- quantileplot("newdeaths.nyt", data=cs) + ylab("Weekly Deaths") + ggtitle("NYT Weekly Deaths") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+
+  
+fctp <- quantileplot("newdeaths.ctp", data=cs) + ylab("Deaths") + ggtitle("CTP") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03")) # +  xlim(as.Date("2020-03-10"),Sys.Date())
+fnyt <- quantileplot("newdeaths.nyt", data=cs) + ylab("Deaths") + ggtitle("NYT") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+fjhu <- quantileplot("newdeaths.jhu", data=cs) + ylab("Deaths") + ggtitle("JHU") + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+pdf(paste(outdir,"weekdeaths_q.pdf", sep="/")) 
+grid.arrange(fnyt,fjhu,fctp)
+dev.off()
+
+
 ## Tests
 figc <- ggplot(cs, aes(x=date, y=totalTestResults, colour=state)) + geom_line(alpha=1) +
   xlim(as.Date("2020-03-10"),Sys.Date()) + scale_y_log10() + plottheme +
@@ -150,6 +209,19 @@ fign <- ggplot(cs, aes(x=date, y=newtests, colour=state)) + geom_line(alpha=1) +
 pdf(paste(outdir,"test.pdf", sep="/"))
 grid.arrange(figc,fign)
 dev.off()
+ 
+figc <- quantileplot("totalTestResults", data=cs) + ggtitle("Total Cumulative Tests") + ylab("Tests")  + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03")) # +  xlim(as.Date("2020-03-10"),Sys.Date())
+fign <- quantileplot("newtests", data=cs) + ggtitle("New Tests in the Past Week") + ylab("Tests")  + scale_y_log10() + xlim(as.Date("2020-03-10"),as.Date("2020-06-03"))
+ pdf(paste(outdir,"test_q.pdf", sep="/"))  
+ grid.arrange(figc,fign)
+dev.off()
+
+
+
+pdf(paste(outdir,"casesdeaths_q.pdf", sep="/")) 
+grid.arrange(fnyt_daily,fnyt_weekly,fnyt_death_weekly,fign,  ncol=2, nrow=2)
+dev.off()
+
 
 ##########################################################################################
 ## Policies
